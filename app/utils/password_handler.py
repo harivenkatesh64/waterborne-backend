8b0eba9dc65d22312d1ccb2484abfedfd39d45ce
harivenkatesh64 <harivenkatesh463@gmail.com>
2026-03-03 15:59:20 +0530

    Fixed bcrypt 72 byte password limit issue

--- app/utils/password_handler.py
+++ app/utils/password_handler.py
@@ -2,8 +2,10 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password: str):
+def hash_password(password: str) -> str:
+    password = password[:72]   # bcrypt limit
     return pwd_context.hash(password)
 
-def verify_password(password: str, hashed_password: str):
-    return pwd_context.verify(password, hashed_password)
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    plain_password = plain_password[:72]
+    return pwd_context.verify(plain_password, hashed_password)
